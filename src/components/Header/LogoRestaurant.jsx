5317b41db28061842ac5052f8c5dde36d81c9375
Manuel Vallejos <manuel.vallejos@interseguro.com.pe>
2023-05-01 02:29:41 -0500

    Add header structure and some styling

---
+++ src/components/Header/LogoRestaurant.jsx
@@ -0,0 +1,44 @@
+const LogoRestaurant = (props) => {
+    return (
+        <svg
+            data-name="Capa 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1158.3 382.4"
+            {...props}
+        >
+            <path d="M923.3 93.15c-1.36-13.32-26.56-22.53-63.19-25.31-3.5-20.53-7.55-44.22-8.74-50.66-2.72-14.75-22.14-22.99-36.57-12.36-7.57 5.58-14.34 5.97-17.92 5.97s-7.57.79-17.92-5.97c-15-9.81-33.85-2.39-36.56 12.36-1.45 7.87-7.17 41.43-10.93 63.58-36.04 10.16-59.44 24.47-58.06 37.95 2.08 20.32 59.7 31.09 128.7 24.03 69.02-7.06 123.27-29.25 121.18-49.58zm-185.35 4.86l3.98-20.71s70.89 11.15 110.72-9.56l3.98 17.52S813.62 112.34 737.95 98z" />
+            <text
+            transform="translate(0 272.3)"
+            fontFamily="Desyrel,Desyrel"
+            fontSize="300px"
+            >
+            <tspan x={0} y={0} letterSpacing="-.04em">
+                {"D"}
+            </tspan>
+            <tspan x={182.4} y={0} letterSpacing={0}>
+                {"e"}
+            </tspan>
+            <tspan x={319.2} y={0} letterSpacing="-.05em">
+                {"B"}
+            </tspan>
+            <tspan x={512.1} y={0} letterSpacing="-.12em">
+                {"a"}
+            </tspan>
+            <tspan x={655.2} y={0} letterSpacing="-.04em">
+                {"r"}
+            </tspan>
+            <tspan x={779.4} y={0} letterSpacing="-.05em">
+                {"r"}
+            </tspan>
+            <tspan x={900.3} y={0} letterSpacing="-.07em">
+                {"i"}
+            </tspan>
+            <tspan x={993.9} y={0} letterSpacing={0}>
+                {"o"}
+            </tspan>
+            </text>
+        </svg>
+    )
+}
+
+export default LogoRestaurant
