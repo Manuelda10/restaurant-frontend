11f3037589531f4aa38c4ce526dd19e077c9992d
Manuel Vallejos <manueldavidv.l@gmail.com>
2023-12-24 05:45:15 -0500

    feat: add header and footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,25 +3,22 @@ import logo from '/logo-restaurant.png'
 
 const Header = () => {
   return(
-    <header className='font-semibold border-b-2 border-gray-300'>
-      <nav className='flex justify-between items-center'>
+    <header className='bg-white font-semibold border-b-2 border-gray-300 sm:sticky sm:top-0'>
+      <nav className='flex justify-center sm:justify-between items-center'>
         <div>
           <Link to="/"><img src={logo} width='180'></img></Link>
         </div>
-        <div>
+        <div className='hidden sm:block'>
           <ul className='flex justify-around items-center'>
-            <li><NavLink to="/" className='bg-orange-400 text-white rounded-xl py-3 px-7 mx-4 hover:bg-orange-500 ease-in duration-300'>Inicio</NavLink></li>
-            <li><NavLink to="/menu"className='mx-4'>Menú</NavLink></li>
-            <li><NavLink to="/favorites" className='mx-4'>Favoritos</NavLink></li>
-            <li><NavLink to="/about" className='mx-4'>Nosotros</NavLink></li>
-          </ul>
+            <li><NavLink to="/" className='bg-orange-500 text-white rounded-xl py-3 px-3 md:px-6 mx-1 md:mx-4 hover:bg-orange-600 ease-in duration-300'>Inicio</NavLink></li>
+            <li><NavLink to="/menu"className='mx-1 md:mx-4'>Menú</NavLink></li>
+            <li><NavLink to="/favorites" className='mx-1 md:mx-4'>Favoritos</NavLink></li>
+            <li><NavLink to="/about" className='mx-1 md:mx-4'>Nosotros</NavLink></li>
+          </ul>   
         </div>
-        <div>
-          <div>
-
-            <Link to='/login' className='bg-blue-950 text-white rounded-xl py-3 px-6 hover:bg-gray-950 ease-in duration-300'>Iniciar sesión</Link>
-          </div>
-        </div>  
+        <div className='hidden sm:block'>
+          <Link to='/login' className='bg-gray-800 text-white rounded-xl py-3 px-3 md:px-4 lg:px-8 hover:bg-gray-950 ease-in duration-300'>Iniciar sesión</Link>
+        </div>
       </nav>
     </header>
   )
