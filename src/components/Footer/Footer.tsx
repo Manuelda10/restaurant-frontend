27e8ee9db18d1f7f503e1ad00eb80edce7236b54
Manuel Vallejos <manueldavidv.l@gmail.com>
2024-03-14 02:48:02 -0500

    feat: improve navegation menu styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,31 +6,31 @@ import cart from '/assets/shopping-cart.svg'
 
 const Footer = () => {
   return(
-    <footer className="sticky bottom-0 border-t-2 rounded-t-lg border-gray-300 bg-gray-950 sm:relative sm:text-center">
+    <footer className="sticky bottom-0 rounded-t-lg border-gray-300 bg-gray-950 sm:relative sm:text-center">
       <nav aria-label="Mobile footer navigation" className="sm:hidden font-semibold">
-        <ul className="grid grid-cols-4 justify-around divide-x-2 divide-gray-300">  
+        <ul id="navbar-list-mobile" className="grid grid-cols-4 justify-around  text-gray-400">  
           <li className="h-full py-1 flex justify-center items-end">
             <NavLink to='/' className="flex flex-col items-center">
                 <img src={home} alt="home-logo" width='35'></img>
-                <p className="text-white" >Inicio</p>
+                <p>Inicio</p>
             </NavLink>
           </li>
           <li className="h-full py-1 flex justify-center items-end">
             <NavLink to='/menu' className="flex flex-col items-center">
                 <img src={chicken} alt="menu-logo" width='35'></img>
-                <p className="text-white">Menú</p>
+                <p>Menú</p>
             </NavLink>
           </li>
           <li className="h-full py-1 flex justify-center items-end">
             <NavLink to='/favorites' className="flex flex-col items-center">
                 <img src={favorite} alt="favoritos-logo" width='32'></img>
-                <p className="text-white">Favoritos</p>
+                <p>Favoritos</p>
             </NavLink>
           </li>
           <li className="h-full py-1 flex justify-center items-end">
             <NavLink to='/about' className="flex flex-col items-center">
                 <img src={cart} alt="carrito-logo" width='32'></img>
-                <p className="text-white">Carrito</p>
+                <p>Carrito</p>
             </NavLink>
           </li>
         </ul>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,21 +3,21 @@ import logo from '/logo-restaurant.png'
 
 const Header = () => {
   return(
-    <header className='bg-white font-semibold border-b-2 border-gray-300 sm:sticky sm:top-0'>
-      <nav className='flex justify-center sm:justify-between items-center'>
-        <div>
+    <header className='bg-white font-semibold border-b-2 border-gray-300 sm:sticky sm:top-0 z-10 '>
+      <nav className='flex justify-center sm:justify-between text-gray-700 items-center'>
+        <div className='flex justify-center items-center'>
           <Link to="/"><img src={logo} width='180'></img></Link>
         </div>
         <div className='hidden sm:block'>
-          <ul className='flex justify-around items-center'>
-            <li><NavLink to="/" className='bg-orange-500 text-white rounded-xl py-3 px-3 md:px-6 mx-1 md:mx-4 hover:bg-orange-600 ease-in duration-300'>Inicio</NavLink></li>
-            <li><NavLink to="/menu"className='mx-1 md:mx-4'>Menú</NavLink></li>
-            <li><NavLink to="/favorites" className='mx-1 md:mx-4'>Favoritos</NavLink></li>
-            <li><NavLink to="/about" className='mx-1 md:mx-4'>Nosotros</NavLink></li>
+          <ul id='navbar-list' className='flex justify-around items-center px-2'>
+            <li><NavLink to="/">Inicio</NavLink></li>
+            <li><NavLink to="/menu">Menú</NavLink></li>
+            <li><NavLink to="/favorites">Favoritos</NavLink></li>
+            <li><NavLink to="/about">Nosotros</NavLink></li>
           </ul>   
         </div>
-        <div className='hidden sm:block'>
-          <Link to='/login' className='bg-gray-800 text-white rounded-xl py-3 px-3 md:px-4 lg:px-8 hover:bg-gray-950 ease-in duration-300'>Iniciar sesión</Link>
+        <div className='hidden sm:flex justify-center items-center'>
+          <Link to='/login' className='bg-gray-800 text-center text-white rounded-md py-3 px-1 md:px-2 lg:px-8 hover:bg-gray-950 ease-in duration-300'>Iniciar sesión</Link>
         </div>
       </nav>
     </header>
